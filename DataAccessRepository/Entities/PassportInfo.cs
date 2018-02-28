87773e675cdd18a8c95f6a9bcc48a7613061a8fb
Manu K M <iammanukm@outlook.com>
2018-02-28 14:49:04 +0530

    Classes added on passport and request details

---
+++ DataAccessRepository/Entities/PassportInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessRepository.Entities
+{
+    class PassportInfo
+    {
+        public int PassportNumber { get; set; }
+        public int VisaNumber { get; set; }
+        public DateTime VisaExpiryDate { get; set; }
+        public DateTime PassportExpiryDate { get; set; }
+    }
+}
