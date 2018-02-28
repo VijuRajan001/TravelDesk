61c9a589d90659c9a35e7f1e239d8ae799d1b6a8
Neethu-Suresh <neethusuresh0123@gmail.com>
2018-02-28 14:47:42 +0530

    classes added on hotel,flight,forex

---
+++ DataAccessRepository/Entities/ForexInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessRepository.Entities
+{
+    class ForexInfo
+    {
+        public int CardNumber { get; set; }
+        public string CountryCode { get; set; }
+        public int MobileNo { get; set; }
+    }
+}
