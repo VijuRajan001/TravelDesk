32c58ad37c462f3202ff6604df500b975e911b41
Manu K M <iammanukm@outlook.com>
2018-03-15 22:33:26 +0530

    Form update

--- DataAccessRepository/Entities/FlightInfo.cs
+++ DataAccessRepository/Entities/FlightInfo.cs
@@ -7,10 +7,12 @@ namespace DataAccessRepository.Entities
     public class FlightInfo
     {
         public long FlightNo { get; set; }
-        public string FlightName { get; set; }
+        public string FlightCarrierName  { get; set; }
         public string To { get; set; }
         public string From { get; set; }
         public double Price { get; set; }
+        public DateTime  FlightDateTime{ get; set; }
+
 
         public int RequestId { get; set; }
         public RequestInfo RequestInfo { get; set; }
