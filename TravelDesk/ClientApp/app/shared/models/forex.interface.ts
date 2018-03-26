c289afbfb03e550020f0776abb4309ff612985d3
nissintkunjumon <nissintkunjumon@gmail.com>
2018-03-26 13:52:50 +0530

    Forex changes

--- TravelDesk/ClientApp/app/shared/models/forex.interface.ts
+++ TravelDesk/ClientApp/app/shared/models/forex.interface.ts
@@ -1,7 +1,33 @@
 
 export class Forex {
-    cardNum: number;
-    countryCode: string;
-    mobileNum: number;
+    constructor(forex: IForex = {
+        id: 0,
+        passportNum: '',
+        visaNum: '',
+        passportExpiryDate: '',
+        visaExpiryDate: '',
+        requestInfoId: 0,
+    }) {
+
+        this.
+
+
+    }
+    public cardNum: number
+    public cardType: string;
+    public cardExpiry: Date;
+
+}
+
+
+
+
+export interface IPassport {
+    cardNum: number
+    cardType: string;
+    cardExpiry: Date;
+
+}
+    
     
 }
