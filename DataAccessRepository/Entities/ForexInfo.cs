c0c6ca865690d813dce09c5ada55abb368561ba5
Viju Rajan <vijurajan001@gmail.com>
2018-03-27 11:46:13 +0530

    Merging Data Access Repo

--- DataAccessRepository/Entities/ForexInfo.cs
+++ DataAccessRepository/Entities/ForexInfo.cs
@@ -7,9 +7,11 @@ namespace DataAccessRepository.Entities
     public class ForexInfo
     {
         public int Id { get; set; }
+
         public string CardNumber { get; set; }
         public string CardType { get; set; }
         public DateTime? CardExpiryDate { get; set; }
+        
         public int RequestInfoId { get; set; }
         public RequestInfo RequestInfo { get; set; }
     }
